be0523c7b1587f7cd40ec58d4114166155593bbc
yasser <yezzat2020@gmail.com>
2019-11-24 19:10:52 +0200

    prepupulate users and questions tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -1,27 +1,24 @@
 <?php
 
 namespace App;
-use App/User;
 use Illuminate\Support\Str;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
-{
-   
-   protect $fillable=['title','body'];
+{   
+   protected $fillable=['title','body'];
     public function user(){
         return $this->belongsto(User::class);
     }
 
-
+    //Mutator
     public function setTitleAttribute($value){
 
     	$this->attributes['title']=$value;
-    	$this->attributes['slug']=Str::slug($value);
-    	
-
+    	$this->attributes['slug']=Str::slug($value);   
     }
+    
    
 }
 
---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Question::class, function (Faker $faker) {
+    return [
+        'title'=>$faker->sentence(rand(5,7)),
+        'body'=>$faker->paragraphs(rand(3,7),true),
+        'views'=>rand(0,10),
+        'anwsers'=>rand(0,10),
+        'votes'=>rand(-3,10)
+
+    ];
+});
